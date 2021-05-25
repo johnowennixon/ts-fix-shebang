e7a30fbf42762b13a0e5ac755128cde5f4ed4996
John Owen Nixon <john.owen.nixon@gmail.com>
2021-05-25 14:59:16 +0100

    Use glob

--- src/ts-fix-shebang.ts
+++ src/ts-fix-shebang.ts
@@ -1,41 +1,46 @@
 #!/usr/bin/env node
 
+import {ArgumentParser} from "argparse"
 import * as fs from "fs"
-import {JsonObject} from "type-fest"
+import * as glob from "glob"
 
-const LF = "\n"
-const SHEBANG_OTHER = "#!"
-const SHEBANG_NODE = "#!/usr/bin/env node"
-
-const packages = JSON.parse(fs.readFileSync("package.json", "utf8")) as JsonObject
-
-const paths: string[] = []
-
-if (packages?.bin) {
-  if (typeof packages.bin === "string") {
-    paths.push(packages.bin)
-  }
-  if (typeof packages.bin === "object") {
-    for (const value of Object.values(packages.bin)) {
-      paths.push(value as string)
-    }
-  }
+interface ParsedArgs {
+  dist: string
 }
 
-for (const path of paths) {
+function parse_args(): ParsedArgs {
+  const ap = new ArgumentParser({description: "Fix TypeScript shebang", add_help: true})
+
+  ap.add_argument("dist")
+
+  return ap.parse_args() as ParsedArgs
+}
+
+function fix_shebang(path: string): void {
   console.log(`Fixing shebang for ${path}`)
 
-  const text_in = fs.readFileSync(path, "utf8")
+  const text_in = fs.readFileSync(path, {encoding: "utf8"})
 
   let lines = text_in.split(/\r?\n/)
 
+  const SHEBANG_OTHER = "#!/"
+  const SHEBANG_NODE = "#!/usr/bin/env node"
+
   if (lines.length && lines[0].startsWith(SHEBANG_OTHER)) {
-    lines.shift()
+    lines[0] = SHEBANG_NODE
   }
 
-  lines.unshift(SHEBANG_NODE + LF)
+  const LF = "\n"
 
   const text_out = lines.join(LF) + LF
 
-  fs.writeFileSync(path, text_out, "utf8")
+  fs.writeFileSync(path, text_out, {encoding: "utf8"})
+}
+
+const pa = parse_args()
+
+const paths = glob.sync(pa.dist, {nodir: true})
+
+for (const path of paths) {
+  fix_shebang(path)
 }
